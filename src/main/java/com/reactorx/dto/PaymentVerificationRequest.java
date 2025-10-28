b89a8f6facb08ca2dddd0c6389caf88413f5a51f
Binore Mohapatra <binore.mohapatra@gmail.com>
2025-10-29 03:09:49 +0530

    first commit

--- src/main/java/com/reactorx/dto/PaymentVerificationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.reactorx.dto;
-import lombok.Data;
-
-@Data
-public class PaymentVerificationRequest {
-    private String razorpay_order_id;
-    private String razorpay_payment_id;
-    private String razorpay_signature;
-}
